e177013a3d99533a8baa854c77d8a031aa6e9cc8
yashaaa16 <158138236+yashaaa16@users.noreply.github.com>
2025-02-03 18:33:40 +0800

    Update WorkEx.jsx

--- src/Components/WorkEx.jsx
+++ src/Components/WorkEx.jsx
@@ -3,7 +3,7 @@ function WorkEx() {
   return (
     <section class="box-border heigth-100  border-2 p-4 width-100 ">
       <h2>Work Experience</h2>
-      <p>Software Developer at XYZ Corp  (2022-Present)</p>
+      <p>Software Developer at XYZ Corp (2022-Present)</p>
       <p>Junior Developer at ABC Inc. (2020-2022)</p>
     </section>
   );
